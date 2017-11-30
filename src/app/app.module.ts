3135d93995b2ce19a1704d9a56c28c5927ec42c8
riki-r <rikihavlin@gmail.com>
2017-11-30 16:58:45 +0200

    First Page

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { FirstPageComponent } from './first-page/first-page.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FirstPageComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

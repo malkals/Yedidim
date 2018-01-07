d9858e38c8d9fd0f2a7fdbf3ffcc7d06117eea90
riki-r <rikihavlin@gmail.com>
2018-01-07 17:11:50 +0200

    fixing login issues

--- src/app/first-page/first-page.component.ts
+++ src/app/first-page/first-page.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from "@angular/router";
-import {FirebaseService} from '../service/firebase/firebase.service' ; 
+import { FirebaseService } from '../service/firebase/firebase.service';
 import { AngularFireAuth } from 'angularfire2/auth';
 import * as firebase from 'firebase/app';
 
@@ -11,41 +11,49 @@ import * as firebase from 'firebase/app';
 })
 export class FirstPageComponent implements OnInit {
 
-  constructor( public router:Router ,public firebaseService: FirebaseService, afAuth:AngularFireAuth  ) {
-  
+  constructor(public router: Router, public firebaseService: FirebaseService, afAuth: AngularFireAuth) {
+
   }
-    login() : void
-    {
-      this.firebaseService.login().then(user=>
-        {
-          
-          this.router.navigate(["main-volunteer"]);
-        })
-    }
+  login(): void {
+    this.firebaseService.login().then(user => {
+      if (!user) {
+        this.router.navigate(["profile"]);
+      }
+      else
+        // console.log(user)
+        this.router.navigate(["volunteer-page"]);
+    });
+  }
 
 
-    exsist_volun(): void{
-      this.firebaseService.login().then(user=>
-        {
-          console.log(user)
-          
-          this.router.navigate(["volunteer-page"]);
-        })
-    }
-       
-
-
-   request_help(): void
-   {
-     this.router.navigate(["request"]);
-   }
-new_volun(): void{
-  this.firebaseService.login().then(user=>
-    {
-      this.router.navigate(["profile"]);
+  exsist_volun(): void {
+    this.firebaseService.login().then(user => {
+      if (!user) {
+        this.router.navigate(["profile"]);
+      }
+      else
+        // console.log(user)
+        this.router.navigate(["volunteer-page"]);
     })
-}
-   
+  }
+
+
+
+  request_help(): void {
+    this.router.navigate(["request"]);
+  }
+  new_volun(): void {
+    this.firebaseService.login().then(user => {
+      if(!user){
+
+      
+      this.router.navigate(["profile"]);
+      }
+      else
+      this.router.navigate(["volunteer-page"]);
+    })
+  }
+
 
   ngOnInit() {
   }
